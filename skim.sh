85038999e21f635f1c0860ea20ceb633d2332838
Silvio <silvio.donato@cern.ch>
2021-02-08 21:31:31 +0100

    fix in skim.sh

--- skim.sh
+++ skim.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 source /cvmfs/sft.cern.ch/lcg/views/LCG_99/x86_64-centos7-gcc8-opt/setup.sh
 
 PATH=$PATH:/cvmfs/cms.cern.ch/slc7_amd64_gcc700/external/llvm/8.0.1/bin/
@@ -14,9 +16,6 @@ token
 
 echo "Check xrootd", root -b -l -q  root://eoscms.cern.ch//store/group/upgrade/delphes_output/YR_Delphes/Delphes342pre14/VBFHToMuMu_M125_14TeV_powheg_pythia8_200PU/VBFHToMuMu_M125_14TeV_powheg_pythia8_1.root -e 'Delphes->Draw("","")'
 root -b -l -q  root://eoscms.cern.ch//store/group/upgrade/delphes_output/YR_Delphes/Delphes342pre14/VBFHToMuMu_M125_14TeV_powheg_pythia8_200PU/VBFHToMuMu_M125_14TeV_powheg_pythia8_1.root -e 'Delphes->Draw("","")'
-root -b -l -q root://eos/cms/store/group/upgrade/delphes_output/YR_Delphes/Delphes342pre15/GluGluHToMuMu_M125_14TeV_powheg_pythia8_200PU/GluGlu_HToMuMu_M125_14TeV_powheg_pythia8_1.root -e 'Delphes->Draw("","")'
-root -b - l -q root://eos/cms/store/group/upgrade/delphes_output/YR_Delphes/Delphes342pre15_hadd/EWKZ2Jets_ZToLL_M-50_14TeV-madgraph-pythia8_200PU/EWKZ2Jets_ZToLL_M-50_13TeV-madgraph-pythia8_1.root -e 'Delphes->Draw("","")'
 
 echo python3.8 converter.py $1 $2 
 python3.8 converter.py $1 $2
-
